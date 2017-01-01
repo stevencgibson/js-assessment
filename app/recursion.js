c18bd89c18a3cf09d8b20319c6d0aa8966d41ea0
Steven Gibson <gib.screenmedia@gmail.com>
2017-01-01 18:05:02 +0000

    Should be able to return a list of files from the data.

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    let res = [];
 
+    function readDir(data) {
+      data.files.forEach((item) => {
+        if (typeof item === 'object') {
+          readDir(item)
+          return;
+        }
+
+        res.push(item);
+      });
+    }
+
+    readDir(data);
+
+    return res;
   },
 
   permute: function(arr) {

edee1ec14148801e97ed31983224e8a4ea0000c2
Steven Gibson <gib.screenmedia@gmail.com>
2017-01-06 16:05:59 +0000

    Should count from start number to end number, one per 1/10th of a second.

--- app/count.js
+++ app/count.js
@@ -2,6 +2,15 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    function log(n) {
+      console.log(n);
 
+      if (n < end) {
+        n++;
+        setTimeout(() => { log(n); }, 100);
+      }
+    }
+
+    log(start);
   }
 };

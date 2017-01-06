cebf1cecce54a19a2a28d79e254e68a17c895880
Steven Gibson <gib.screenmedia@gmail.com>
2017-01-06 14:37:24 +0000

    Should be able to return a list of files in a subdir.

--- app/recursion.js
+++ app/recursion.js
@@ -3,16 +3,25 @@ exports = typeof window === 'undefined' ? global : window;
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
     let res = [];
+    let dirTree = [];
 
-    function readDir(data) {
-      data.files.forEach((item) => {
+    function readDir(dir) {
+      dirTree.push(dir.dir);
+
+      dir.files.forEach((item) => {
         if (typeof item === 'object') {
-          readDir(item)
+          readDir(item);
+          return;
+        }
+
+        if (dirName && dirTree.indexOf(dirName) === -1) {
           return;
         }
 
         res.push(item);
       });
+
+      dirTree.pop();
     }
 
     readDir(data);

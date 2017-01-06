f798689b6dd57231db190479c6f574e87f41e7c7
Steven Gibson <gib.screenmedia@gmail.com>
2017-01-06 16:08:02 +0000

    Should provide a method to cancel the counting.

--- app/count.js
+++ app/count.js
@@ -2,15 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    var timer;
+
     function log(n) {
       console.log(n);
 
       if (n < end) {
         n++;
-        setTimeout(() => { log(n); }, 100);
+        timer = setTimeout(() => { log(n); }, 100);
       }
     }
 
     log(start);
+
+    return {
+      cancel: function() {
+        clearTimeout(timer);
+      }
+    };
   }
 };

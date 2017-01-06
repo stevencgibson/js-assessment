746675cdee84b1807ce40449ed23584465fd0153
Steven Gibson <gib.screenmedia@gmail.com>
2017-01-06 16:17:18 +0000

    Should be able to alter the context in which a method runs.

--- app/objects.js
+++ app/objects.js
@@ -2,7 +2,7 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   alterObjects: function(constructor, greeting) {
